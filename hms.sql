d9beeb604d06ceec6b9526fd4f5f60d1e756ce4b
Seher Gürel <37286447+shrgrl@users.noreply.github.com>
2019-12-24 00:49:22 +0300

    Update hms.sql

--- hms.sql
+++ hms.sql
@@ -16,15 +16,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `hms`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
+CREATE DATABASE hms;
+USE hms;
 
 CREATE TABLE IF NOT EXISTS `admin` (
   `id` int(11) NOT NULL,
@@ -121,16 +114,7 @@ CREATE TABLE IF NOT EXISTS `doctorslog` (
 --
 
 INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
-(1, 2, 'sarita@gmail.com', 0x30000000000000000000000000000000, '2017-01-06 05:53:31', '', 1),
-(2, 0, 'admin', 0x3a3a3100000000000000000000000000, '2017-01-06 06:36:07', '', 0),
-(3, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-06 06:36:37', '06/01/2017 07:36:45', 1),
-(4, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-06 06:41:33', '12:11:46', 1),
-(5, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-06 06:55:16', '06-01-2017 12:27:47 PM', 1),
-(6, 0, 'admin', 0x3a3a3100000000000000000000000000, '2017-01-06 07:07:12', '', 0),
-(7, 0, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-07 08:04:42', '', 0),
-(8, 0, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-07 08:04:55', '', 0),
-(9, 2, 'sarita@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-07 08:05:54', '07-01-2017 01:36:28 PM', 1);
-
+(2, 0, 'admin', admin123, '2017-01-06 06:36:07', '', 0);
 -- --------------------------------------------------------
 
 --
@@ -149,15 +133,10 @@ CREATE TABLE IF NOT EXISTS `doctorspecilization` (
 --
 
 INSERT INTO `doctorspecilization` (`id`, `specilization`, `creationDate`, `updationDate`) VALUES
-(1, 'Gynecologist/Obstetrician', '2016-12-28 06:37:25', ''),
-(2, 'General Physician', '2016-12-28 06:38:12', ''),
-(3, 'Dermatologist', '2016-12-28 06:38:48', ''),
-(4, 'Homeopath', '2016-12-28 06:39:26', ''),
-(5, 'Ayurveda', '2016-12-28 06:39:51', ''),
-(6, 'Dentist', '2016-12-28 06:40:08', ''),
-(7, 'Ear-Nose-Throat (Ent) Specialist', '2016-12-28 06:41:18', ''),
-(9, 'Demo test', '2016-12-28 07:37:39', '28-12-2016 01:28:42 PM'),
-(10, 'Bones Specialist demo', '2017-01-07 08:07:53', '07-01-2017 01:38:04 PM');
+(1, 'Cildiye', '2016-12-28 06:37:25', ''),
+(2, 'Dahiliye', '2016-12-28 06:38:12', ''),
+(3, 'Çocuk Cerrahi', '2016-12-28 06:38:48', ''),
+(4, 'Psikiyatri', '2016-12-28 06:39:26', '');
 
 -- --------------------------------------------------------
 
@@ -175,28 +154,7 @@ CREATE TABLE IF NOT EXISTS `userlog` (
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `userlog`
---
 
-INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
-(6, 1, '', 0x3a3a3100000000000000000000000000, '2017-01-06 07:02:28', '', 1),
-(7, 1, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-06 07:04:28', '', 1),
-(8, 0, 'admin', 0x3a3a3100000000000000000000000000, '2017-01-06 07:07:41', '06-01-2017 12:38:09 PM', 0),
-(9, 1, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-06 07:08:01', '', 1),
-(10, 1, 'info@w3gang.com', 0x3a3a3100000000000000000000000000, '2017-01-06 07:10:09', '06-01-2017 12:41:43 PM', 1),
-(11, 2, 'test@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-07 07:57:18', '07-01-2017 01:27:34 PM', 1),
-(12, 0, 'asdad', 0x3a3a3100000000000000000000000000, '2017-01-07 07:57:44', '', 0),
-(13, 0, 'xyz@test.com', 0x3a3a3100000000000000000000000000, '2017-01-07 07:59:43', '', 0),
-(14, 5, 'amit12@gmail.com', 0x3a3a3100000000000000000000000000, '2017-01-07 08:00:44', '07-01-2017 01:34:19 PM', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `fullName` varchar(255) NOT NULL,
   `address` longtext NOT NULL,
@@ -207,21 +165,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `regDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updationDate` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
-(1, 'Anuj kumar', 'Test address', 'Aligarh', 'Males', 'info@w3gang.com', 'f925916e2754e5e03f75dd58a5733251', '2016-12-26 07:03:09', '30-12-2016 11:27:47 AM'),
-(2, 'Sarita pandey', 'New Delhi India', 'Delhi', 'female', 'test@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2016-12-30 05:34:39', ''),
-(3, 'Amit', 'New Delhi', 'New delhi', 'male', 'amit@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 06:36:53', ''),
-(4, 'Rahul Singh', 'New Delhi', 'New delhi', 'male', 'rahul@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 07:41:14', ''),
-(5, 'Amit kumar', 'New Delhi India', 'Delhi', 'male', 'amit12@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2017-01-07 08:00:26', '07-01-2017 01:32:12 PM');
-
---
--- Indexes for dumped tables
---
 
 --
 -- Indexes for table `admin`
